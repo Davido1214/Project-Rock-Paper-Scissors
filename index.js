6be1c89a72c8939230048baed1dc1da421b43ec5
Davido1214 <oleadavid1214@gmail.com>
2024-10-06 04:41:40 -0700

    Added function that uses switch cases to determine if the user wins the game or not

--- index.js
+++ index.js
@@ -15,18 +15,81 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+
 
 function getHumanChoice() {
    return prompt("Choose Rock Paper or Scissors")
 }
 
-console.log(getHumanChoice())
+
 
 
 function playRound(humanChoice, computerChoice){
 
-    humanChoice
+   let humanAnswer = humanChoice.toLowerCase()
+
+switch(computerChoice){
+    case "rock":
+        console.log("computer chose rock!")
+
+        if(humanAnswer === "paper"){
+            console.log("Paper beats Rock! Human Wins!");
+            humanScore+=1;
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }else if(humanAnswer === "rock"){
+            console.log("its a tie!")
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }else{
+            console.log("Computer Wins!")
+            computerScore+=1
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }
+        
+        break;
+    case "paper":
+        console.log("computer chose Paper!")
+
+        if(humanAnswer === "scissors"){
+            console.log("Scissors beats Paper! Human Wins!");
+            humanScore+=1;
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }else if(humanAnswer === "paper"){
+            console.log("its a tie!")
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }else{
+            console.log("Computer Wins!")
+            computerScore+=1
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }
+        
+        break;
+    case "scissors":
+        console.log("computer chose Scissors!")
+
+        if(humanAnswer === "rock"){
+            console.log("Rock beats Scissors! Human Wins!");
+            humanScore+=1;
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }else if(humanAnswer === "paper"){
+            console.log("its a tie!")
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }else{
+            console.log("Computer Wins!")
+            computerScore+=1
+            console.log("Human Score: "+ humanScore)
+            console.log("Computer Score: "+ computerScore)
+        }
+        break;
+    
+}
     
 }
 
